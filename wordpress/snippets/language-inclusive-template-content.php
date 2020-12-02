90fd6a89b28a1b366b24531813aa78f8a88cca71
Mico <9155442+Miconen@users.noreply.github.com>
2020-12-02 10:53:54 +0200

    Create language-inclusive-template-content.php

---
+++ wordpress/snippets/language-inclusive-template-content.php
@@ -0,0 +1,12 @@
+// Template language inclusive content
+add_action('template_redirect', function () {
+    $currentLanguage = function_exists('pll_current_language') ? pll_current_language() : '';
+    if (
+        is_page_template('templates/works.php') ||
+        (is_page_template('templates/works.php') &&
+            $currentLanguage === 'fi')
+    ) {
+        add_action('wpex_hook_content_top', function () {
+        });
+    };
+});
