7cca8d317c60a2218664429cff003c52e99c8cc5
Mico <9155442+Miconen@users.noreply.github.com>
2021-05-12 12:42:44 +0300

    Create Block.ts

---
+++ ts/Queue/Block.ts
@@ -0,0 +1,21 @@
+export default class Block {
+	public x: number;
+	public y: number;
+	public width: number;
+	public height: number;
+	public color: string;
+
+	constructor(
+		x: number,
+		y: number,
+		width: number,
+		height: number,
+		color: string
+	) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+}
