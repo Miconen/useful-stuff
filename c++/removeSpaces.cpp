cabe770a50eb8b2b5e30fa5f9fae48808bbced4c
Mico <9155442+Miconen@users.noreply.github.com>
2021-06-02 10:59:42 +0300

    Create removeSpaces.cpp

---
+++ c++/removeSpaces.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+string removeSpaces(string str)
+{
+	str.erase(remove(str.begin(), str.end(), ' '), str.end());
+	return str;
+}
+
+main()
+{
+	string input{};
+	removeSpaces(getline(cin, input));
+  return 0;
+}
